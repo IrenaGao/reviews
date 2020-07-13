9471aebd0d89f970ff04d64eea99985f5312cd49
Chen <andrewchen349@gmail.com>
2020-07-12 21:45:30 -0400

    Started firebase implementation

--- App.js
+++ App.js
@@ -1,18 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-//Firebase app
+//Firebase imports 
 import { app } from './src/Config';
+import 'firebase/firestore';
 
-export default function App() {
+//Navigations
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { materialBottomTabNavigator, createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+//Import Screens
+import Home from './screens/Home';
+import Register from './screens/Register';
+import Login from './screens/Login';
+
+
+//Creating Navigation Stacks
+const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+export default class App extends React.Component {
+  uid = app.auth().currentUser?.uid;
+  ref = app.firestore().doc('/users/' + this.uid);
+
+  state = {
+    isLoading : true,
+    user: null,
+  }
+
+  componentDidMount = async () => {
+    app.auth().onAuthStateChanged(async user => {
+      if(user){
+       let uid = app.auth().currentUser?.uid;
+       this.setState({user: user});
+      }
+      else{
+        this.setState({user : null});
+      }
+
+      if(this.state.isLoading === true){
+        this.setState({isLoading: false});
+      }
+    })
+  }
+
+  render(){
+    if(this.state.loading === true){
+      return null;
+    }
+    if(this.state.user !== null){
+      return homeStack();
+    }
+    else{
+      return loginStack();
+    }
+    // return (
+    //   <View style={styles.container}>
+    //     <Text>Open up App.js to start working on your app!</Text>
+    //   </View>
+    // );
+  }
+}
+
+function homeStack(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+function loginStack(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
